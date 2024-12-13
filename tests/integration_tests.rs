f7bd7159577c20de701df3c0bbccfb60b4485a6c
pepemon42069 <pepemon42069@proton.me>
2024-12-13 19:05:58 +0000

    added another test

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -30,4 +30,51 @@ mod integration_tests {
             _ => panic!("expected ExecutionResult::Executed with FillResult::Created"),
         }
     }
+
+    #[test]
+    fn orderbook_flow_place_2_limit_ask_orders() {
+        let mut orderbook = OrderBook::default();
+
+        let test_order_1 = LimitOrder::new(1, 100, 100, Side::Bid);
+        let operation_1 = Operation::Limit(test_order_1);
+
+        let test_order_2 = LimitOrder::new(2, 110, 200, Side::Ask);
+        let operation_2 = Operation::Limit(test_order_2);
+        
+        let execution_result_1 = orderbook.execute(operation_1);
+        let execution_result_2 = orderbook.execute(operation_2);
+
+        let (retrieved_order_1, _) = orderbook.order_store.get(test_order_1.id).unwrap();
+        let (retrieved_order_2, _) = orderbook.order_store.get(test_order_2.id).unwrap();
+        
+        let expected_max_bid = orderbook.get_max_bid();
+        let expected_min_ask = orderbook.get_min_ask();
+        let expected_stats = orderbook.stats();
+
+
+        match (execution_result_1, execution_result_2) {
+            (
+                ExecutionResult::Executed(FillResult::Created(created_order_1)),
+                ExecutionResult::Executed(FillResult::Created(created_order_2)),
+            ) => {
+                let assert_order_flow = || {
+                    assert_eq!(created_order_1, test_order_1);
+                    assert_eq!(created_order_2, test_order_2);
+                    assert_eq!(*retrieved_order_1, test_order_1);
+                    assert_eq!(*retrieved_order_2, test_order_2);
+                    assert_eq!(expected_max_bid, Some(100));
+                    assert_eq!(expected_min_ask, Some(110));
+                    assert_eq!(
+                        expected_stats,
+                        vec![
+                            (100, 100, Side::Bid),
+                            (110, 200, Side::Ask)
+                        ]
+                    );
+                };
+                assert_order_flow();
+            },
+            _ => panic!("expected ExecutionResult::Executed with FillResult::Created for both orders"),
+        }
+    }
 }
